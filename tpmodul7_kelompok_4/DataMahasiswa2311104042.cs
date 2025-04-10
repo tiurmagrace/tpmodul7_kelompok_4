e69b20a306b9acfb639ec6348184a04e8de71e0e
Kinn <candradinata956@gmail.com>
2025-04-10 19:00:38 +0700

    Merge Fix

---
+++ tpmodul7_kelompok_4/DataMahasiswa2311104042.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa2311104042
+{
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string filePath = "tp7_1_2311104042.json";
+
+        string jsonString = File.ReadAllText(filePath);
+
+        Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+        Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+        Console.ReadLine();
+
+    }
+}
--- tpmodul7_kelompok_4/Program.cs
+++ tpmodul7_kelompok_4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        var mhs = new DataMahasiswa2311104053();
+        mhs.ReadJSON();
+
+         var kuliah = new KuliahMahasiswa2311104053();
+        kuliah.ReadJSON();
+
+         DataMahasiswa2311104042.ReadJSON();
+        KuliahMahasiswa2311104042.ReadJSON(); 
+        Console.ReadLine();
+
         Console.WriteLine("=== JSON 1 ===");
         DataMahasiswa2311104061.ReadJSON();
         
