6a8295452a3cb8cbe616fd8cba18d32df35a1a3f
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-12 19:14:48 +0700

    menambahkan class DataMahasiswa2311104073

---
+++ tpmodul7_kelompok_4/DataMahasiswa2311104073.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa2311104073
+{
+    public string nama { get; set; }
+    public string nim { get; set; }
+    public string fakultas { get; set; }
+
+    public static void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("tp7_1_2311104073.json");
+        DataMahasiswa2311104073 data = JsonSerializer.Deserialize<DataMahasiswa2311104073>(jsonString);
+        Console.WriteLine($"Nama {data.nama} dengan nim {data.nim} dari fakultas {data.fakultas}");
+    }
+}
---
+++ tpmodul7_kelompok_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Memanggil method ReadJSON dari class DataMahasiswa2311104073
+        DataMahasiswa2311104073.ReadJSON();
+
+        // Kalau ingin sekalian panggil bagian kedua (kuliah), bisa tambahkan ini juga:
+        // KuliahMahasiswa2311104073.ReadJSON();
+    }
+}
