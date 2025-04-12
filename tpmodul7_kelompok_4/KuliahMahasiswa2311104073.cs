dc91532db1a180f5de5d6cf4d35a5517b2803da8
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-12 19:32:22 +0700

    menambahkan class KuliahMahasiswa2311104073

---
+++ tpmodul7_kelompok_4/KuliahMahasiswa2311104073.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class Matkul
+{
+    public string kode { get; set; }
+    public string nama { get; set; }
+}
+
+public class KuliahMahasiswa2311104073
+{
+    public List<Matkul> mataKuliah { get; set; }
+
+    public static void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("tp7_2_2311104073.json");
+        KuliahMahasiswa2311104073 data = JsonSerializer.Deserialize<KuliahMahasiswa2311104073>(jsonString);
+
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        int i = 1;
+        foreach (var mk in data.mataKuliah)
+        {
+            Console.WriteLine($"MK {i} {mk.kode} - {mk.nama}");
+            i++;
+        }
+    }
+}
---
+++ tpmodul7_kelompok_4/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        KuliahMahasiswa2311104073.ReadJSON();
+    }
+}
